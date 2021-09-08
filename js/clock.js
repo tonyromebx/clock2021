444c14883d24344aea65b80ab43683f7ee08404f
zitechlo <tonyromebx@gmail.com>
2021-09-08 00:22:55 -0400

    code almost done 9/7/21

--- js/clock.js
+++ js/clock.js
@@ -1,19 +1,36 @@
 
+function bestTime () {
+    const today = new Date();
+    const hour = today.getHours() ;
+    const minutes = today.getMinutes();
+    const showSec = today.getSeconds();
 
-const today = new Date();
-const hour = today.getHours() ;
-const minutes = today.getMinutes();
+    const convertHour = (realTime) => {
+        if (realTime === 0) {
+            return 12
+        }else if(realTime > 12){
+            return realTime - 12
+        }else{
+            return realTime
+        }
+    };
+
+    const trueMin = (min) => {
+        const num = 0;
+        const num2 = "";
+        return  min < 10 ? num : num2 + minutes
+    };
+
+    document.getElementById('show').innerHTML = ` ${convertHour(hour)} : ${trueMin(minutes)} : ${showSec}`;
+}
+
+const mySec = setInterval(bestTime,1000);
 
 
-const time = (realTime) => {
-    if(realTime > 12) {
-        return realTime - 12
-    }else {
-        return realTime
-    }
-};
 
-document.getElementById('hour').innerHTML = `${time(hour)}:${minutes}`;
+
+
+
 
 
 
